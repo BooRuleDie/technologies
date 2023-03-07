9d92e8b6624f129659f93fa640a215b9edffadf9
BooRuleDie <booruledie+github@gmail.com>
2023-03-07 15:52:37 +0300

    select added

---
+++ MySQL/select.php
@@ -0,0 +1,33 @@
+<?php 
+    $hostname = "localhost";
+    $username = "root";
+    $password = "mysql";
+    $dbname = "createdDB";
+
+    try {
+        // create connection
+        $conn = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
+
+        // set the PDO error mode to exception
+        $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        // fetch data and print
+        $query = $conn -> query("SELECT * from Guests;");
+        // set Fetch mode to associative array (dictionay, key-value pair) it's easier to work with data like that
+        $query -> setFetchMode(PDO::FETCH_ASSOC);
+        $data = $query -> fetchAll();
+        
+        foreach ($data as $array) {
+            foreach ($array as $key => $value) {
+                echo "$key: $value<br>";
+            }      
+            echo "<br>";
+        }
+        
+    } catch(PDOException $e) {
+        echo "<br>" . $e -> getMessage();
+    }
+    
+    // closing connection
+    $conn = null;
+?>
