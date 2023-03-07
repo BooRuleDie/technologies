45861cd2a2fd3bcab50eebc1c3dfc98c4122e357
BooRuleDie <booruledie+github@gmail.com>
2023-03-07 13:41:06 +0300

    create table added

---
+++ MySQL/create-table.php
@@ -0,0 +1,34 @@
+<?php 
+    $hostname = "localhost";
+    $username = "root"; // be sure that user has enough permission to create a database
+    $password = "mysql";
+    $dbname = "createdDB";
+
+    try {
+        // create connection
+        // using PDO instead of mysqli this time
+        $conn = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
+
+        // set the PDO error mode to exception
+        $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        // create guests table 
+        $sql = "CREATE TABLE Guests (
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            firstname VARCHAR(30) NOT NULL,
+            lastname VARCHAR(30) NOT NULL,
+            email VARCHAR(30),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        )";
+
+        $conn -> exec($sql);
+
+        echo "Guests Table has been created!<br>";
+        
+    } catch(PDOException $e) {
+        echo $sql . "<br>" . $e -> getMessage();
+    }
+    
+    // closing connection
+    $conn = null;
+?>
