35258c67fff4882015ed3c633957aeaf27dc07a7
BooRuleDie <booruledie+github@gmail.com>
2023-03-07 13:42:55 +0300

    create table added

--- MySQL/create-table.php
+++ MySQL/create-table.php
@@ -12,7 +12,7 @@
         $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
         // create guests table 
-        $sql = "CREATE TABLE Guests (
+        $sql = "CREATE TABLE IF NOT EXISTS Guests (
             id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             firstname VARCHAR(30) NOT NULL,
             lastname VARCHAR(30) NOT NULL,
