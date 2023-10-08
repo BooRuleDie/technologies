5dab324bf2e6a5c9ef6299301bb33a66724e233d
BooRuleDie <booruledie+github@gmail.com>
2023-10-08 15:10:23 +0300

    first app completed

--- React/tutorial/src/index.js
+++ React/tutorial/src/index.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const Greeting = () => {
-    return <h1>Hello World!</h1>;
+const BookList = () => {
+    return (
+        <div className="booklist">
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </div>
+    );
+};
+
+const Book = () => {
+    return (
+        <div className="book">
+            <img
+                src="https://picsum.photos/200/300"
+                alt="random image from lorem image"
+            />
+            <h2>Lorem ipsum dolor</h2>
+            <p
+                style={{
+                    opacity: "50%",
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                }}
+            >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Inventore, dolorum?
+            </p>
+        </div>
+    );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<Greeting />);
+root.render(<BookList />);
