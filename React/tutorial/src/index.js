58762086d6ae2b52fc32d77a0a89ceeedb2fa0a0
BooRuleDie <booruledie+github@gmail.com>
2023-10-28 11:43:38 +0300

    map iteration covered (with children)

--- React/tutorial/src/index.js
+++ React/tutorial/src/index.js
@@ -4,33 +4,57 @@ import "./index.css";
 
 const book_db = [
     {
+        id: 1,
         title: "some title",
         author: "some author",
         src: "https://picsum.photos/200/300",
     },
-];
-// destructuring
-const { title, author, src } = book_db[0];
-
-const BookList = () => {
-    return (
-        <div className="booklist">
-            <Book title={title} author={author} src={src} />
-            <Book title={title} author={author} src={src} />
-            <Book title={title} author={author} src={src}>
-                <hr style={{margin: "1rem 0"}}/>
+    {
+        id: 2,
+        title: "some title 1",
+        author: "some author 1",
+        src: "https://picsum.photos/200/301",
+    },
+    {
+        id: 3,
+        title: "some title 2",
+        author: "some author 2",
+        src: "https://picsum.photos/200/302",
+        children: (
+            <>
+                <hr style={{ margin: "1rem 0" }} />
                 <p>
                     Thanks to{" "}
                     <span style={{ fontWeight: "bold" }}>children</span> prop, I
                     can create this special paragraph.
                 </p>
-            </Book>
-            <Book title={title} author={author} src={src} />
-            <Book title={title} author={author} src={src} />
-            <Book title={title} author={author} src={src} />
-            <Book title={title} author={author} src={src} />
-            <Book title={title} author={author} src={src} />
-            <Book title={title} author={author} src={src} />
+            </>
+        ),
+    },
+    {
+        id: 4,
+        title: "some title 3",
+        author: "some author 3",
+        src: "https://picsum.photos/200/303",
+    },
+];
+// destructuring
+// const { title, author, src } = book_db[0];
+
+const BookList = () => {
+    return (
+        <div className="booklist">
+            {book_db.map(({ id, title, author, src, children }) => {
+                return (
+                    <Book
+                        key={id}
+                        title={title}
+                        author={author}
+                        src={src}
+                        children={children}
+                    />
+                );
+            })}
         </div>
     );
 };
