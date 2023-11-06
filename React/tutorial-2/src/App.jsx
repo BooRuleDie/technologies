c2980f294dd1a305dacf3dca5e8c7a6c16e5a48e
BooRuleDie <booruledie+github@gmail.com>
2023-11-06 17:27:44 +0300

    React Challange 03-02 completed

--- React/tutorial-2/src/App.jsx
+++ React/tutorial-2/src/App.jsx
@@ -25,9 +25,11 @@
 // import Starter from "./tutorial/02-useEffect/starter/04-fetch-data";
 // import Final from "./tutorial/02-useEffect/final/04-fetch-data";
 
-import Starter from "./tutorial/03-conditional-rendering/starter/01-multiple-returns-basics";
-import Final from "./tutorial/03-conditional-rendering/final/01-multiple-returns-basics";
+// import Starter from "./tutorial/03-conditional-rendering/starter/01-multiple-returns-basics";
+// import Final from "./tutorial/03-conditional-rendering/final/01-multiple-returns-basics";
 
+import Starter from "./tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data";
+import Final from "./tutorial/03-conditional-rendering/final/02-multiple-returns-fetch-data";
 
 function App() {
     return (
--- React/tutorial-2/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ React/tutorial-2/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,54 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import { useEffect, useState } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+    const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch(url)
+                .then((response) => {
+                    // if response is not 200
+                    if (!response.ok) {
+                        setIsLoading(false);
+                        setIsError(true);
+                        return <h1>Something Went Wrong</h1>;
+                    }
+                    return response.json();
+                })
+                .then((data) => {
+                    setIsLoading(false);
+                    setUser(data);
+                })
+                .catch((error) => {
+                    console.error(error);
+                    setIsLoading(false);
+                    setIsError(true);
+                });
+        }, 3000);
+    }, []);
+
+    if (isLoading) {
+        return <h1>Loading...</h1>;
+    }
+
+    if (isError) {
+        return <h1>Something Went Wrong</h1>;
+    }
+
+    if (user) {
+        return (
+            <div>
+                <img
+                    src={user.avatar_url}
+                    style={{ width: "8rem", borderRadius: "2rem" }}
+                />
+                <h4>{user.bio}</h4>
+                <h2>Works at {user.company}</h2>
+            </div>
+        );
+    }
 };
 export default MultipleReturnsFetchData;
