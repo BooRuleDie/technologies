e2719904a01842bc195f2dc9e566ad528f5da418
BooRuleDie <booruledie+github@gmail.com>
2023-11-07 07:55:30 +0300

    React Challange 03-06 completed

--- React/tutorial-2/src/App.jsx
+++ React/tutorial-2/src/App.jsx
@@ -37,15 +37,18 @@
 // import Starter from "./tutorial/03-conditional-rendering/starter/04-short-circuit-overview";
 // import Final from "./tutorial/03-conditional-rendering/final/04-short-circuit-overview";
 
-import Starter from "./tutorial/03-conditional-rendering/starter/05-short-circuit-examples";
-import Final from "./tutorial/03-conditional-rendering/final/05-short-circuit-examples";
+// import Starter from "./tutorial/03-conditional-rendering/starter/05-short-circuit-examples";
+// import Final from "./tutorial/03-conditional-rendering/final/05-short-circuit-examples";
+
+import Starter from "./tutorial/03-conditional-rendering/starter/06-toggle-challenge";
+import Final from "./tutorial/03-conditional-rendering/final/06-toggle-challenge";
 
 
 function App() {
     return (
         <div className="container">
             <Starter />
-            <Final />
+            {/* <Final /> */}
         </div>
     );
 }
--- React/tutorial-2/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ React/tutorial-2/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+    const [isToggledDown, setIsToggledDown] = useState(false);
+    return (
+        <>
+            <button
+                className="btn"
+                onClick={() => {
+                    setIsToggledDown(!isToggledDown);
+                }}
+            >
+                Toggle
+            </button>
+            {isToggledDown && <SomeElement />}
+        </>
+    );
+};
+
+const SomeElement = () => {
+    return (
+        <>
+            <h2 style={{ margin: "2rem 0" }}>Hello From Some Element</h2>
+        </>
+    );
 };
 
 export default ToggleChallenge;
