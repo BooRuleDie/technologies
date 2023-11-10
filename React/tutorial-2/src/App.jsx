03376d78edd56f8943c00a728851f5db7b42221f
BooRuleDie <booruledie+github@gmail.com>
2023-11-10 13:39:27 +0300

    React Challange 02-05 completed

--- React/tutorial-2/src/App.jsx
+++ React/tutorial-2/src/App.jsx
@@ -43,9 +43,11 @@
 // import Starter from "./tutorial/03-conditional-rendering/starter/06-toggle-challenge";
 // import Final from "./tutorial/03-conditional-rendering/final/06-toggle-challenge";
 
-import Starter from "./tutorial/03-conditional-rendering/starter/07-user-challenge";
-import Final from "./tutorial/03-conditional-rendering/final/07-user-challenge";
+// import Starter from "./tutorial/03-conditional-rendering/starter/07-user-challenge";
+// import Final from "./tutorial/03-conditional-rendering/final/07-user-challenge";
 
+import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function";
+import Final from "./tutorial/02-useEffect/final/05-cleanup-function";
 
 function App() {
     return (
--- React/tutorial-2/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ React/tutorial-2/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,65 @@
+import { useEffect, useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+    const [isClicked, setIsClicked] = useState(false);
+    return (
+        <>
+            <button
+                className="btn"
+                onClick={() => {
+                    setIsClicked(!isClicked);
+                }}
+            >
+                Click
+            </button>
+            {/* useEffect in the SomeComponent will be invoked whenever the SomeComponent mounts */}
+            {isClicked && <EventListenerComponent />}
+        </>
+    );
+};
+
+const SomeComponent = () => {
+    useEffect(() => {
+        console.log("Hello from useEffect within SomeComponent");
+        const interval = setInterval(() => {
+            console.log("hello from interval");
+            // if you don't use a clean-up function here setInterval won't stop even though you unmount the SomComponent Component
+        }, 1000);
+
+        // the syntax of the clean-up function
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+    return (
+        <>
+            <h4 style={{ margin: "2rem 0" }}>Hello from Some Component</h4>
+        </>
+    );
+};
+
+const EventListenerComponent = () => {
+    useEffect(() => {
+        const someFunction = () => {
+            // some logic here
+        };
+
+        // attach the event listener to the window object
+        window.addEventListener("scroll", someFunction);
+
+        // now if you don't use a clean-up function, you'll see lots of event-listeners on your window object after hitting toggle button a few times
+        // in order to avoid it, we need to use a clean-up function again
+        return () => {
+            window.removeEventListener("scroll", someFunction);
+        };
+    }, []);
+    return (
+        <>
+            <h4 style={{ margin: "2rem 0" }}>
+                Hello from Event Listener Component
+            </h4>
+        </>
+    );
 };
 
 export default CleanupFunction;
